9d0bfaf3ee113104b95c7986b95b22dec23b5a0e
Luis Prado <luisprado0095@gmail.com>
2021-09-09 18:45:58 -0300

    a

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig:{
+    apiKey: "AIzaSyDAQNcPXuqOXtphEH0VuCxkDkqvBEH5Le4",
+    authDomain: "sala-juegos-prado.firebaseapp.com",
+    projectId: "sala-juegos-prado",
+    storageBucket: "sala-juegos-prado.appspot.com",
+    messagingSenderId: "701922913936",
+    appId: "1:701922913936:web:8c3d847a099396d224b215"
+  },
 };
